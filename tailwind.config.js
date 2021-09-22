55658668545987be1b1d16753895b192c63fd1f6
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2021-09-21 19:13:05 -0500

    Reverting to Next's defaults for tailwind imports

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 module.exports = {
     purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-    darkMode: "class", // or 'media' or 'class'
+    darkMode: "class",
     theme: {
-        extend: {},
+        extend: {}
     },
     variants: {
-        extend: {},
+        extend: {}
     },
-    plugins: [],
+    plugins: []
 };
