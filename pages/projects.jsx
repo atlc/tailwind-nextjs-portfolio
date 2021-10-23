ebfd8e6f6613707a43cdf52c42e5db2672e0478d
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2021-10-23 18:10:55 -0500

    Have Swal rendering video tags nicely for media examples

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -1,8 +1,14 @@
+import Image from "next/image";
+import { MdPhotoCamera } from "react-icons/md";
+import { useSelector } from "react-redux";
+import { selectColor } from "../store/reducers";
 import Layout from "../components/Layout";
 import CardContainer from "../components/CardContainer";
 import Card from "../components/Card";
-import { selectColor } from "../store/reducers";
-import { useSelector } from "react-redux";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+
+const SwalWRC = withReactContent(Swal);
 
 const LGQuote = ({ children }) => (
     <>
@@ -14,6 +20,43 @@ const LGQuote = ({ children }) => (
 
 const Experience = () => {
     const color = useSelector(selectColor);
+
+    const SwalWithImages = async links => {
+        const images = links.map(link => {
+            if (link.includes("mp4")) {
+                return {
+                    html: (
+                        <video autoPlay loop muted>
+                            <source src={link} type="video/mp4" />
+                        </video>
+                    )
+                };
+            } else {
+                return { imageUrl: link };
+            }
+        });
+
+        const queue = SwalWRC.mixin({
+            showClass: {
+                backdrop: `swal2-noanimation`,
+                popup: "",
+                icon: ""
+            },
+            hideClass: {
+                popup: ""
+            },
+            confirmButtonText: "Next Image",
+            progressSteps: images.map((item, index) => index + 1)
+        });
+
+        for await (const [index, img] of images.entries()) {
+            if (index === images.length - 1) {
+                await queue.fire({ ...img, confirmButtonText: "Close" });
+            } else {
+                await queue.fire({ ...img, currentProgressStep: index });
+            }
+        }
+    };
 
     const projects = [
         {
@@ -78,12 +121,54 @@ const Experience = () => {
             title: "Arduino",
             description: [
                 <div>
-                    <p>Smoke Detector</p>
-                    <p>Anglerfish [pics with modal?]</p>
+                    <div className="flex">
+                        <p className="mr-2">
+                            <LGQuote>Arduino Smoke Detector</LGQuote>
+                        </p>
+                        <button
+                            className="text-3xl"
+                            onClick={() =>
+                                SwalWithImages([
+                                    "https://raw.githubusercontent.com/atlc/arduino-smoke-detector/master/demo.gif",
+                                    "https://raw.githubusercontent.com/atlc/arduino-smoke-detector/master/schematic.png"
+                                ])
+                            }>
+                            <MdPhotoCamera />
+                        </button>
+                    </div>
+                    <p>
+                        I promise that we're using approved smoke/CO detectors in our residence. I was curious to see how inexpensively and
+                        easily a Smoke/CO/Gas detector could produced for, with wide open possibilities for accessibility purposes.
+                    </p>
+                </div>,
+                <div>
+                    <div className="flex">
+                        <p className="mr-2">Anglerfish [pics with modal?]</p>
+                        <button
+                            className="text-3xl"
+                            onClick={() => SwalWithImages(["https://i.imgur.com/eMRZGFy.mp4", "https://i.imgur.com/q9Eytam.mp4"])}>
+                            <MdPhotoCamera />
+                        </button>
+                    </div>
                     <p>Arcade games [pics with modal?]</p>
                 </div>
             ],
-            link: "https://covalence.io/"
+            link: (
+                <>
+                    <a
+                        className={`text-gray-100 shadow-xl bg-${color}-600 hover:bg-${color}-300 hover:text-${color}-700 px-3 mx-2 py-2 rounded-md text-md font-light`}
+                        target="_blank"
+                        href="https://github.com/atlc/arduino-smoke-detector">
+                        smoke detector
+                    </a>
+                    <a
+                        className={`text-gray-100 shadow-xl bg-${color}-600 hover:bg-${color}-300 hover:text-${color}-700 px-3 mx-2 py-2 rounded-md text-md font-light`}
+                        target="_blank"
+                        href="https://www.npmjs.com/package/shitpostify">
+                        shitpostify
+                    </a>
+                </>
+            )
         }
     ];
 
